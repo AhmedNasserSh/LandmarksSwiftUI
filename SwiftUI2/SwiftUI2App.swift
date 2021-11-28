cef3d21b22de6e1d35bb506694bf7545037763a6
Ahmed Naser <ahmed.nasser2310@gmail.com>
2021-11-28 11:27:12 +0200

    Initial Commit

---
+++ SwiftUI2/SwiftUI2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI2App.swift
+//  SwiftUI2
+//
+//  Created by Ahmed Naser on 28/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
